077f5f717890dac8cb4f43ab6877a20e74d7a896
MattewEon <mattewphoenyx@hotmail.fr>
2017-07-14 11:37:56 +0200

    node_module issues

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {SocialUser} from "./lib/social-user";
 import {SocialLoginService} from "./lib/social-login.service";
-import {FacebookModule} from "./node_modules/ngx-facebook";
+import {FacebookModule} from "ngx-facebook";
 
 export {SocialUser} from "./lib/social-user";
 export {SocialLoginService} from "./lib/social-login.service";
